29369709de7f61850d25caafff83af0b11c52c5c
AlexandreMRM <67433372+AlexandreMRM@users.noreply.github.com>
2024-12-05 16:55:40 -0300

    Update Relatorio_de_Pendencias.py

--- pages/Relatorio_de_Pendencias.py
+++ pages/Relatorio_de_Pendencias.py
@@ -154,3 +154,7 @@ if data_inicial and data_final:
     container_dataframe.write(f'Existem {int(len(df_vendedores_rascunho_filtrado))} pendências.')
 
     container_dataframe.dataframe(df_vendedores_rascunho_filtrado, hide_index=True, use_container_width=True)
+
+'''
+TESTE
+'''
